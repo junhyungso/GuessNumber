15d2d401df167b7480a067e3f0059bd7bf5819aa
junhyungso <junso5551@gmail.com>
2024-09-23 20:08:03 -0400

    delete comment

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -12,7 +12,6 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: 'white',
     textAlign: 'center',
-    // borderWidth: Platform.OS === 'android' ? 2 : 0,
     borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderColor: 'white',
     padding: 12,
